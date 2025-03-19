9a57932d55b4cce891036902465119f25eba48de
unknown <leandrovictor0103@gmail.com>
2025-03-19 18:40:47 -0300

    Adicionando consultas

---
+++ anexos/consultas/Relacao_Sintoma_Positivo.sql
@@ -0,0 +1,110 @@
+SELECT '112020' as MesAno
+      ,COUNT(CASE B00113 WHEN 1 THEN 1 END)  as Diarreia
+      ,COUNT(CASE B0014  WHEN 1 THEN 1 END)  as DificuldadeRespirar
+      ,COUNT(CASE B0015  WHEN 1 THEN 1 END)  as DorCabeca
+      ,COUNT(CASE B0013  WHEN 1 THEN 1 END)  as DorGarganta
+      ,COUNT(CASE B00112 WHEN 1 THEN 1 END)  as DorMuscular
+      ,COUNT(CASE B0016  WHEN 1 THEN 1 END)  as DorPeito
+      ,COUNT(CASE B00110 WHEN 1 THEN 1 END)  as DorOlhos
+      ,COUNT(CASE B0019  WHEN 1 THEN 1 END)  as Fadiga
+      ,COUNT(CASE B0011  WHEN 1 THEN 1 END)  as Febre
+      ,COUNT(CASE B0018  WHEN 1 THEN 1 END)  as NarizEntupidoEscorrendo
+      ,COUNT(CASE B0017  WHEN 1 THEN 1 END)  as Nausea    
+      ,COUNT(CASE B00111 WHEN 1 THEN 1 END)  as PerdaCheiroSabor
+      ,COUNT(CASE B0012  WHEN 1 THEN 1 END)  as Tosse
+      ,COUNT(*) TotalLinhas
+FROM `PNAD_COVID.PNAD_COVID_112020` 
+WHERE CASE 
+                WHEN B009B = 1 THEN 1
+                WHEN B009D = 1 THEN 1
+                WHEN B009F = 1 THEN 1
+            END = 1
+UNION ALL
+SELECT '102020' as MesAno
+      ,COUNT(CASE B00113 WHEN 1 THEN 1 END)  as Diarreia
+      ,COUNT(CASE B0014  WHEN 1 THEN 1 END)  as DificuldadeRespirar
+      ,COUNT(CASE B0015  WHEN 1 THEN 1 END)  as DorCabeca
+      ,COUNT(CASE B0013  WHEN 1 THEN 1 END)  as DorGarganta
+      ,COUNT(CASE B00112 WHEN 1 THEN 1 END)  as DorMuscular
+      ,COUNT(CASE B0016  WHEN 1 THEN 1 END)  as DorPeito
+      ,COUNT(CASE B00110 WHEN 1 THEN 1 END)  as DorOlhos
+      ,COUNT(CASE B0019  WHEN 1 THEN 1 END)  as Fadiga
+      ,COUNT(CASE B0011  WHEN 1 THEN 1 END)  as Febre
+      ,COUNT(CASE B0018  WHEN 1 THEN 1 END)  as NarizEntupidoEscorrendo
+      ,COUNT(CASE B0017  WHEN 1 THEN 1 END)  as Nausea    
+      ,COUNT(CASE B00111 WHEN 1 THEN 1 END)  as PerdaCheiroSabor
+      ,COUNT(CASE B0012  WHEN 1 THEN 1 END)  as Tosse
+      ,COUNT(*) TotalLinhas
+FROM `PNAD_COVID.PNAD_COVID_102020` 
+WHERE CASE 
+                WHEN B009B = 1 THEN 1
+                WHEN B009D = 1 THEN 1
+                WHEN B009F = 1 THEN 1
+            END = 1
+UNION ALL
+SELECT '092020' as MesAno
+      ,COUNT(CASE B00113 WHEN 1 THEN 1 END)  as Diarreia
+      ,COUNT(CASE B0014  WHEN 1 THEN 1 END)  as DificuldadeRespirar
+      ,COUNT(CASE B0015  WHEN 1 THEN 1 END)  as DorCabeca
+      ,COUNT(CASE B0013  WHEN 1 THEN 1 END)  as DorGarganta
+      ,COUNT(CASE B00112 WHEN 1 THEN 1 END)  as DorMuscular
+      ,COUNT(CASE B0016  WHEN 1 THEN 1 END)  as DorPeito
+      ,COUNT(CASE B00110 WHEN 1 THEN 1 END)  as DorOlhos
+      ,COUNT(CASE B0019  WHEN 1 THEN 1 END)  as Fadiga
+      ,COUNT(CASE B0011  WHEN 1 THEN 1 END)  as Febre
+      ,COUNT(CASE B0018  WHEN 1 THEN 1 END)  as NarizEntupidoEscorrendo
+      ,COUNT(CASE B0017  WHEN 1 THEN 1 END)  as Nausea    
+      ,COUNT(CASE B00111 WHEN 1 THEN 1 END)  as PerdaCheiroSabor
+      ,COUNT(CASE B0012  WHEN 1 THEN 1 END)  as Tosse
+      ,COUNT(*) TotalLinhas
+FROM `PNAD_COVID.PNAD_COVID_092020` 
+WHERE CASE 
+                WHEN B009B = 1 THEN 1
+                WHEN B009D = 1 THEN 1
+                WHEN B009F = 1 THEN 1
+            END = 1
+UNION ALL
+SELECT '082020' as MesAno
+      ,COUNT(CASE B00113 WHEN 1 THEN 1 END)  as Diarreia
+      ,COUNT(CASE B0014  WHEN 1 THEN 1 END)  as DificuldadeRespirar
+      ,COUNT(CASE B0015  WHEN 1 THEN 1 END)  as DorCabeca
+      ,COUNT(CASE B0013  WHEN 1 THEN 1 END)  as DorGarganta
+      ,COUNT(CASE B00112 WHEN 1 THEN 1 END)  as DorMuscular
+      ,COUNT(CASE B0016  WHEN 1 THEN 1 END)  as DorPeito
+      ,COUNT(CASE B00110 WHEN 1 THEN 1 END)  as DorOlhos
+      ,COUNT(CASE B0019  WHEN 1 THEN 1 END)  as Fadiga
+      ,COUNT(CASE B0011  WHEN 1 THEN 1 END)  as Febre
+      ,COUNT(CASE B0018  WHEN 1 THEN 1 END)  as NarizEntupidoEscorrendo
+      ,COUNT(CASE B0017  WHEN 1 THEN 1 END)  as Nausea    
+      ,COUNT(CASE B00111 WHEN 1 THEN 1 END)  as PerdaCheiroSabor
+      ,COUNT(CASE B0012  WHEN 1 THEN 1 END)  as Tosse
+      ,COUNT(*) TotalLinhas
+FROM `PNAD_COVID.PNAD_COVID_082020` 
+WHERE CASE 
+                WHEN B009B = 1 THEN 1
+                WHEN B009D = 1 THEN 1
+                WHEN B009F = 1 THEN 1
+            END = 1
+UNION ALL
+SELECT '072020' as MesAno
+      ,COUNT(CASE B00113 WHEN 1 THEN 1 END)  as Diarreia
+      ,COUNT(CASE B0014  WHEN 1 THEN 1 END)  as DificuldadeRespirar
+      ,COUNT(CASE B0015  WHEN 1 THEN 1 END)  as DorCabeca
+      ,COUNT(CASE B0013  WHEN 1 THEN 1 END)  as DorGarganta
+      ,COUNT(CASE B00112 WHEN 1 THEN 1 END)  as DorMuscular
+      ,COUNT(CASE B0016  WHEN 1 THEN 1 END)  as DorPeito
+      ,COUNT(CASE B00110 WHEN 1 THEN 1 END)  as DorOlhos
+      ,COUNT(CASE B0019  WHEN 1 THEN 1 END)  as Fadiga
+      ,COUNT(CASE B0011  WHEN 1 THEN 1 END)  as Febre
+      ,COUNT(CASE B0018  WHEN 1 THEN 1 END)  as NarizEntupidoEscorrendo
+      ,COUNT(CASE B0017  WHEN 1 THEN 1 END)  as Nausea    
+      ,COUNT(CASE B00111 WHEN 1 THEN 1 END)  as PerdaCheiroSabor
+      ,COUNT(CASE B0012  WHEN 1 THEN 1 END)  as Tosse
+      ,COUNT(*) TotalLinhas
+FROM `PNAD_COVID.PNAD_COVID_072020` 
+WHERE CASE 
+                WHEN B009B = 1 THEN 1
+                WHEN B009D = 1 THEN 1
+                WHEN B009F = 1 THEN 1
+            END = 1
+ORDER BY 1
